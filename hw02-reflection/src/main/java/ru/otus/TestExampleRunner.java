af39b891bb4a083ba7fc10693dee00e57632bd8f
drovocek <mycodepg@gmail.com>
2022-08-06 15:27:40 +0300

    2,3 add example+runner

--- hw02-reflection/src/main/java/ru/otus/TestExampleRunner.java
+++ hw02-reflection/src/main/java/ru/otus/TestExampleRunner.java
@@ -3,9 +3,9 @@ package ru.otus;
 /**
  * To start the application:
  * ./gradlew build
- * java -jar ./hw02-reflection/build/libs/gradleTestRunner-0.1.jar
+ * java -jar ./hw02-reflection/build/libs/gradleTestExampleRunner-0.1.jar
  */
-public class TestRunner {
+public class TestExampleRunner {
 
     public static void main(String[] args) {
         System.out.println("!!!!!!!!!!");

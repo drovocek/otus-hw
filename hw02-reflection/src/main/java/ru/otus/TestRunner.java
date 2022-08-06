b10dffbff6cc5694037bb7f5c63dd7481f50345b
drovocek <mycodepg@gmail.com>
2022-08-06 15:25:44 +0300

    1.create three annotations

---
+++ hw02-reflection/src/main/java/ru/otus/TestRunner.java
@@ -0,0 +1,13 @@
+package ru.otus;
+
+/**
+ * To start the application:
+ * ./gradlew build
+ * java -jar ./hw02-reflection/build/libs/gradleTestRunner-0.1.jar
+ */
+public class TestRunner {
+
+    public static void main(String[] args) {
+        System.out.println("!!!!!!!!!!");
+    }
+}

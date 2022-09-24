f922c94e4a6825ca27b14658aeec7705a3756007
drovocek <mycodepg@gmail.com>
2022-09-24 15:24:26 +0300

    done

--- hw08-serialization/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw08-serialization/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,33 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
+    /**
+     * 'Gson instances are Thread-safe'
+     * <a href="https://www.javadoc.io/doc/com.google.code.gson/gson/2.8.0/com/google/gson/Gson.html">Doc</a>
+     * */
+    private final Gson gson;
+    private final String fileName;
+
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
+        this.gson = new Gson();
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
-        //формирует результирующий json и сохраняет его в файл
+        String json = this.gson.toJson(data);
+        try (var writer = new BufferedWriter(new FileWriter(this.fileName))) {
+            writer.write(json);
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
--- hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,17 +1,41 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import ru.otus.model.Measurement;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ResourcesFileLoader implements Loader {
 
+    /**
+    * 'Gson instances are Thread-safe'
+    * <a href="https://www.javadoc.io/doc/com.google.code.gson/gson/2.8.0/com/google/gson/Gson.html">Doc</a>
+    * */
+    private final Gson gson;
+    private final String fileName;
+
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
+        this.gson = new Gson();
     }
 
     @Override
     public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
-        return null;
+        String fullFilePath = Objects.requireNonNull(getClass().getClassLoader().getResource(this.fileName)).getPath();
+
+        try (var reader = new JsonReader(new FileReader(fullFilePath))) {
+            Type listType = new TypeToken<ArrayList<Measurement>>() {
+            }.getType();
+            return this.gson.fromJson(reader, listType);
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }

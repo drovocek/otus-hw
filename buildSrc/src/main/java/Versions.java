d03e46cef9e1a2072f4cbcd4bab1fbbc6bf0d84d
drovocek <mycodepg@gmail.com>
2022-10-09 09:18:54 +0300

    starter

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -3,4 +3,7 @@ public interface Versions {
     String springBoot = "2.7.2";
     String protobuf = "3.21.1";
     String glassfishJson = "2.0.1";
+    String gson = "2.9.1";
+    String jetty = "11.0.7";
+    String freemarker = "2.3.31";
 }
